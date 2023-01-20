c077c3b2b386b2890d924734297b659700f9eb1c
firminoemanuel08 <117324442+firminoemanuel08@users.noreply.github.com>
2023-01-20 14:35:38 -0300

    4

--- Js/formulário.js
+++
@@ -1,7 +0,0 @@
-const camposDoFormulario = document.querySelectorAll('[required]')
-
-const listaREspostas = {
-    "nome": e.target.elements["nome"].value,
-    "email": e.target.elements["email"].value,
-    "mensagem": e.target.elements["mensagem"].value
-}
